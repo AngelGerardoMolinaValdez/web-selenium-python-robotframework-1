4ad7366e26afe36dd1ae175edfe34a41d8390d4e
Angel Gerardo Molina Valdez <angelgerardomolinavaldez@gmail.com>
2023-10-20 08:30:55 -0600

    Iteraciones por filas en el archivo de datos

--- run.cmd
+++ run.cmd
@@ -1,17 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Limpieza de pantalla
-cls
+:: VARIABLES
+set testdata_file=data/steps/test_data.csv
+set /a line_count=0
 
-:: Establece el número total de iteraciones
-set "max_iterations=0"
+:: cuenta las líneas en el archivo CSV
+for /f %%a in (%testdata_file%) do (
+    set /a max_iterations+=1
+)
+set /a max_iterations-=2
 
 :: Bucle que va de 0 a n (max_iterations)
 for /l %%i in (0,1,%max_iterations%) do (
+    set "results_output_dir=results/result-%%i"
+    mkdir "!results_output_dir!"
     :: Ejecuta las pruebas en la carpeta steps
     robot ^
-        --name "Test crear cuenta y transferir fondos" ^
+        --name "Test crear cuenta y transferir fondos"^
         --metadata robot:6.1.1^
         --metadata python:3.11^
         --metadata chrome:118^
@@ -19,8 +25,8 @@ for /l %%i in (0,1,%max_iterations%) do (
         --include creacion-transferencias ^
         --variable ITERATION:%%i ^
         --variablefile ./data/variables/globals.yml ^
-        --outputdir ./results/ ^
-        --report creacion-transferencias ^
+        --outputdir "!results_output_dir!" ^
+        --log creacion-transferencias ^
         --output creacion-transferencias ^
         tests/steps/
 )
