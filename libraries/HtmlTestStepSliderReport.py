435115a83fd83c32f27e82dc7d88508550db6cf8
Angel Gerardo Molina Valdez <angelgerardomolinavaldez@gmail.com>
2024-03-05 06:56:31 -0600

    Se establecen reportes de ejecucion estables

--- libraries/HtmlTestStepSliderReport.py
+++ libraries/HtmlTestStepSliderReport.py
@@ -1,23 +1,23 @@
 """
-En este listener es posible generar un reporte de los pasos de ejecución de un test case. Para agregar un paso al reporte, se debe agregar el tag STEP: en la keyword o en el mensaje de log. El mensaje se puede usar para reportar información adicional de un paso, como por ejemplo, el valor de una variable, el resultado de una operación, etc.
+En este listener es posible generar un reporte de pasos con imágenes. Para agregar una imagen a un paso, se debe agregar el tag STEP:IMAGE: en el mensaje. El mensaje se puede usar para reportar información adicional de un paso, como por ejemplo, el valor de una variable, el resultado de una operación, etc.
 
-El reporte se genera en la carpeta output/reports/style_2/ y se genera un archivo HTML por cada test que se ejecute.
+El reporte se genera en la carpeta output/reports/report_step_slider/ y se genera un archivo HTML por cada test que se ejecute.
 
 Para utilizar el listener, se debe especificar como listener al ejecutar las pruebas:
-- robot --listener HtmlTestStepsReport2.py tests
+- robot --listener HtmlTestStepSliderReport.py tests
 
 Ejemplo:
     *** Keywords ***
     My Keyword
-        [Tags]  STEP:DESCRIPCIÓN DEL PASO:INFO
+        [Tags]  STEP:IMAGE:DESCRIPCIÓN DEL PASO:INFO
         No Operation
 
     My Other Keyword
-        [Tags]  STEP:DESCRIPCIÓN DEL PASO 2
+        [Tags]  STEP:IMAGE:DESCRIPCIÓN DEL PASO 2
         No Operation
 
     My Other Keyword
-        [Tags]  STEP:DESCRIPCIÓN DEL PASO 3:FAIL
+        [Tags]  STEP:IMAGE:DESCRIPCIÓN DEL PASO 3:FAIL
         No Operation
 
 Los estatus posibles son:
@@ -33,15 +33,17 @@ El valor predeterminado es INFO.
 
 También es posible utilizar la keyword Log con la misma estructura de tags.
 
-Consideraciones:
-- Si bien se puede usar STEP: para definir un paso también se tomara en cuenta STEP:IMAGE: para agregar un paso en este reporte pero no tomara la captura de pantalla, esto con el fin de poder reutilizar la misma descripción de paso en diferentes reportes.
+Este reporte tiene la misma estructura que el HtmlTestStepsReport2.py, pero con la diferencia de que se agrega una imagen a los pasos.
 """
 import os
 import re
 import datetime
 from jinja2 import Environment, FileSystemLoader
+from PIL import Image, ImageGrab
+import base64
+import io
 
-class HtmlTestStepsReport2:
+class HtmlTestStepSliderReport:
     ROBOT_LISTENER_API_VERSION = 2
 
     def __init__(self):
@@ -60,6 +62,8 @@ class HtmlTestStepsReport2:
         if not os.path.exists(os.path.join(self.base_path, "output", "robot")):
             os.mkdir(os.path.join(self.base_path, "output", "robot"))
 
+        self.execution_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "output", "reports"))
+
     def start_test(self, name, attrs):
         self.current_test = {
             'name': name
@@ -67,19 +71,19 @@ class HtmlTestStepsReport2:
 
     def end_test(self, name, attrs):
         today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        path_to_report = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "output", "reports", self.current_test["name"] + " " + today.replace(":", "-")))
+        path_to_report = os.path.abspath(os.path.join(self.execution_path, self.current_test["name"] + " " + today.replace(":", "-")))
 
         if not os.path.exists(path_to_report):
             os.mkdir(path_to_report)
 
-        path_to_report = os.path.join(path_to_report, "style_2")
+        path_to_report = os.path.join(path_to_report, "report_step_slider")
         os.mkdir(path_to_report)
 
         env = Environment(loader=FileSystemLoader(
             os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "assets", "static", 'templates'))
             )
         )
-        template = env.get_template('step_report_2.html')
+        template = env.get_template('step_report_3.html')
         output = template.render(testname=self.current_test["name"], steps_data=self.keywords_config + self.keywords_data)
         with open(os.path.join(path_to_report, self.current_test["name"] + ".html"), "w", encoding="utf-8") as f:
             f.write(output)
@@ -91,14 +95,16 @@ class HtmlTestStepsReport2:
             self.keyword_config = True
 
     def end_keyword(self, name, attrs):
-        tag_message_title = re.search(r"^STEP:(?:IMAGE:)?(.+?)(?::(INFO|PASS|CRITICAL|FAIL|FALTA|WARNING|DEBUG))?(?:===|:|$)", "===>".join(attrs['tags']))
+        tag_message_title = re.search(r"STEP:IMAGE:(.+?)(?::(INFO|PASS|CRITICAL|FAIL|FALTA|WARNING|DEBUG))?(?:===|:|$)", "===>".join(attrs['tags']))
 
         if tag_message_title:
+            image_content = self.__take_screenshot()
             step_data = {}
             level = tag_message_title.group(2) if tag_message_title.group(2) else "INFO"
             step_title = tag_message_title.group(1)
             step_data["step_level"] = level.lower()
             step_data["step_title"] = step_title
+            step_data["step_image"] = image_content
 
             if self.keyword_config:
                 self.keywords_config.append(step_data)
@@ -109,16 +115,31 @@ class HtmlTestStepsReport2:
             self.keyword_config = False
 
     def log_message(self, message):
-        tag_message_title = re.search(r"^STEP:(?:IMAGE:)?(.+?)(?::(INFO|PASS|CRITICAL|FAIL|FALTA|WARNING|DEBUG))?(?:===|:|$)", message['message'])
+        tag_message_title = re.search(r"STEP:IMAGE:(.+?)(?::(INFO|PASS|CRITICAL|FAIL|FALTA|WARNING|DEBUG))?(?:===|:|$)", message['message'])
 
         if tag_message_title:
+            image_content = self.__take_screenshot()
             step_data = {}
             level = tag_message_title.group(2) if tag_message_title.group(2) else "INFO"
             step_title = tag_message_title.group(1)
             step_data["step_level"] = level.lower()
             step_data["step_title"] = step_title
+            step_data["step_image"] = image_content
 
             if self.keyword_config:
                 self.keywords_config.append(step_data)
             else:
                 self.keywords_data.append(step_data)
+
+    def __take_screenshot(self):
+        image_path = os.path.join(self.execution_path,"imagen.png")
+
+        im1 = ImageGrab.grab()
+        im1.save(image_path)
+
+        im = Image.open(image_path)
+        buff = io.BytesIO()
+        im.save(buff, format="PNG")
+        os.remove(image_path)
+
+        return base64.b64encode(buff.getvalue()).decode("utf-8")
