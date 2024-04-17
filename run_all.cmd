810442a79e8b2f29d1bc5a7c0af0f7a1522b14b1
Angel Gerardo Molina Valdez <angelgerardomolinavaldez@gmail.com>
2024-04-17 04:13:06 -0600

    actualizacion de ruta de listener en archivo bat de ejecucion

--- run_all.cmd
+++ run_all.cmd
@@ -1,1 +1,1 @@
-poetry run robot --outputdir output/robot --listener ./libraries/HtmlTestStepSliderReport.py tests
+poetry run robot --outputdir output/robot --listener ./config/execution/HtmlTestReportStepSlider.py tests
